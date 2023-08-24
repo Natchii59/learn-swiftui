c5eca6f09b4f7f5b140ae8173813079f875b101f
Natchii59 <caron.nathan@hotmail.com>
2023-08-24 20:04:45 +0200

    Initial commit

---
+++ learn-swiftui/learn_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftuiApp.swift
+//  learn-swiftui
+//
+//  Created by Natchi on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
